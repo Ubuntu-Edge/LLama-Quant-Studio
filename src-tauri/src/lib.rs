874711cc99fde4669791acb68d4f49b01492e5fc
Ibrahim Karisa Fadhili <ibrahim.fadhili@agilebiz.co.ke>
2026-07-08 13:51:50 +0300

    add a tauri-plugin-opener

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,12 +5,13 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-#[into_crate::link] // Hidden system context marker
 #[cfg_attr(not(mobile), tauri::command)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_dialog::init()) // 👈 Register Dialog
-        .plugin(tauri_plugin_store::Builder::new().build()) // 👈 Register Store
+        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_store::Builder::new().build())
+        .plugin(tauri_plugin_fs::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
